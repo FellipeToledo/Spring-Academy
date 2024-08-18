6991c16dbd1007ff7b68b1be41c1b94c5989a3f4
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-08-18 14:43:12 -0300

    Testing Basic Authentication

--- cashcard/src/main/java/com/springacademy/cashcard/CashCard.java
+++ cashcard/src/main/java/com/springacademy/cashcard/CashCard.java
@@ -8,5 +8,7 @@ import org.springframework.data.annotation.Id;
 public record CashCard(
         @Id
         Long id,
-        Double amount) {
+        Double amount,
+        String owner
+) {
 }
--- cashcard/src/test/java/com/springacademy/cashcard/CashCardJsonTest.java
+++ cashcard/src/test/java/com/springacademy/cashcard/CashCardJsonTest.java
@@ -26,15 +26,15 @@ public class CashCardJsonTest {
     private final ClassPathResource resource = new ClassPathResource("example/cashcard/expected.json");
     private final ClassPathResource resourceList = new ClassPathResource("example/cashcard/list.json");
 
-    @Test
-    void myFirstTest() {
-        assertThat(42).isEqualTo(42);
-    }
+    // @Test
+    // void myFirstTest() {
+    // assertThat(42).isEqualTo(42);
+    // }
 
     //GET
     @Test
     void cashCardSerializationTest() throws IOException {
-        CashCard cashCard = new CashCard(99L, 123.45);
+        CashCard cashCard = new CashCard(99L, 123.45, "fellipe");
         assertThat(json.write(cashCard)).isStrictlyEqualToJson(resource);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
@@ -47,13 +47,14 @@ public class CashCardJsonTest {
     @Test
     void cashCardDeserializationTest() throws IOException {
         String expected = """
-           {
-               "id":99,
-               "amount":123.45
-           }
-           """;
+                {
+                    "id": 99,
+                    "amount": 123.45,
+                    "owner": "fellipe"
+                }
+                """;
         assertThat(json.parse(expected))
-                .isEqualTo(new CashCard(99L, 123.45));
+                .isEqualTo(new CashCard(99L, 123.45, "fellipe"));
         assertThat(json.parseObject(expected).id()).isEqualTo(99);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
@@ -68,9 +69,9 @@ public class CashCardJsonTest {
     void cashCardListDeserializationTest() throws IOException {
         String expected="""
          [
-            { "id": 99, "amount": 123.45 },
-            { "id": 100, "amount": 1.00 },
-            { "id": 101, "amount": 150.00 }
+                     {"id": 99, "amount": 123.45 , "owner": "fellipe"},
+                     {"id": 100, "amount": 1.00 , "owner": "fellipe"},
+                     {"id": 101, "amount": 150.00, "owner": "fellipe"}
          ]
          """;
         assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
@@ -79,8 +80,8 @@ public class CashCardJsonTest {
     @BeforeEach
     void setUp() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00));
+                new CashCard(99L, 123.45, "fellipe"),
+                new CashCard(100L, 1.00, "fellipe"),
+                new CashCard(101L, 150.00, "fellipe"));
     }
 }
