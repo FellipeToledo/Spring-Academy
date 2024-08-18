e530b9aefac2460f035231b8e77b9db1bbab5836
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-08-18 15:44:50 -0300

    Repository, Controller and Creation method Updates

--- cashcard/src/main/java/com/springacademy/cashcard/CashCardRepository.java
+++ cashcard/src/main/java/com/springacademy/cashcard/CashCardRepository.java
@@ -1,5 +1,7 @@
 package com.springacademy.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -9,4 +11,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface CashCardRepository extends
         CrudRepository<CashCard, Long>,
         PagingAndSortingRepository<CashCard, Long> {
+
+    CashCard findByIdAndOwner(Long id, String owner);
+    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
+
 }
